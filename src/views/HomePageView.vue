b49f5a0d698494083df57ea117187da962d54fc9
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-12-03 09:50:42 +0100

    Feat: Add homepage view

---
+++ src/views/HomePageView.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable @typescript-eslint/ban-ts-comment -->
+<script lang="ts" setup>
+//@ts-ignore
+import Hero from '@/components/homePage/HeroComponent.vue'
+//@ts-ignore
+import FromBlog from '@/components/homePage/FromBlogComponent.vue'
+//@ts-ignore
+import AboutUs from '@/components/homePage/AboutUsComponent.vue'
+//@ts-ignore
+import Testimony from '@/components/homePage/TestimonyComponent.vue'
+//@ts-ignore
+import Footer from '@/components/homePage/FooterComponent.vue'
+//@ts-ignore
+import Contact from '@/components/homePage/ContactComponent.vue'
+//@ts-ignore
+import Counter from '@/components/homePage/CounterComponent.vue'
+</script>
+<template>
+    <Hero/>
+    <AboutUs/>
+    <Counter/>
+    <FromBlog/>
+    <Testimony/>
+    <Contact/>
+    <Footer/>
+   
+</template>
+<style scoped>
+</style>
