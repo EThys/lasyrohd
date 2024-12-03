c478c64d939189044e7eed23fa0876f3e2144899
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-12-03 09:55:33 +0100

    Feat: Add default file

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+  <!-- <Nav />
+  </template>
+  <script setup lang="ts">
+  const name = 'App'
+  import Nav from './components/Nav.vue'
+  </script> -->
+</template>
+<script setup lang="ts"></script>
